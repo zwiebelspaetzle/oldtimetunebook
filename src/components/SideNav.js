e87ed0a31e50ea9599a7c16cf68a39647cee0b10
Chris Ward <cward@ddm.io>
2018-06-15 08:00:20 -0600

    sliding sidenav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -16,7 +16,6 @@ class SideNav extends Component {
   state = {collapsed: false}
 
   handleToggleClick = () => {
-    console.log(!this.state.collapsed);
     this.setState({collapsed: !this.state.collapsed})
   }
 
@@ -25,12 +24,16 @@ class SideNav extends Component {
       let tuneFile = tune.replace(/ /g, '_').replace(/'/g, '');
       return <Link key={key} to={"/tunes/"+tuneFile}><li>{tune}</li></Link>
     })
-    let className = (this.state.collapsed) ? 'sideNav collapsed' : 'sideNav';
+    let sideNavClass = (this.state.collapsed) ? 'sideNav collapsed' : 'sideNav';
     return (
-      <div className={className}>
+      <div className={sideNavClass}>
+        <div className="menu">
+            <Link to="/"><li>Home</li></Link>
+            { tuneList }
+        </div>
+        <div className="toggleButtonContainer">
           <span className="toggleButton" onClick={this.handleToggleClick}><FontAwesomeIcon icon={faChevronLeft} /></span>
-          <Link to="/"><li>Home</li></Link>
-          { tuneList }
+        </div>
       </div>
     )
   }
