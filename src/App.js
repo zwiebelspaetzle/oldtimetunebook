07e61fa550b124ab02a312cf8d196e3f6488aee1
Chris Ward <cward@ddm.io>
2018-08-21 09:34:32 -0600

    Revert "Merge pull request #11 from zwiebelspaetzle/side-menu-squeeze"
    This reverts commit 01169b2542101f76c58d47ab4d0262da30cd9598, reversing
    changes made to 4b7ad9088048f2a5c2508ca31f6da0d3f6c60d30.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import faChevronLeft from '@fortawesome/fontawesome-free-solid/faChevronLeft'
 
 import Home from './components/Home'
+import OsmdContainer from './components/OsmdContainer'
 import SideNav from './components/SideNav'
-import TuneContainer from './components/TuneContainer'
 import './App.css'
 
-
 class App extends Component {
-  state = {sideNavCollapsed: true}
-
-  componentDidMount () {
-    // show menu when loading to home page
-    if (window.location.href.indexOf('tunes') === -1) {
-      this.setState({sideNavCollapsed: false})
-    }
-  }
-
-  handleSideNavToggleClick = () => {
-    this.setState({sideNavCollapsed: !this.state.sideNavCollapsed})
-  }
-
   render () {
-    let sideNavToggleButtonClass = (this.state.sideNavCollapsed) ? 'sideNavToggleButton collapsed' : 'sideNavToggleButton';
     return (
       <Router>
-        <div className="wrapper">
-          <SideNav collapsed={this.state.sideNavCollapsed}/>
-          <div className="sideNavToggleButtonContainer">
-            <span className={sideNavToggleButtonClass} onClick={this.handleSideNavToggleClick}><FontAwesomeIcon icon={faChevronLeft} /></span>
-          </div>
+        <div>
+          <SideNav />
           <Route path='/' exact={true} component={Home} />
           <Route path='/tunes/:tunefile' component={TuneContainer} />
         </div>
       </Router>
     )
   }
+}
+
+const TuneContainer = ({match}) => {
+  return (
+    <div>
+      <OsmdContainer tune={match.params.tunefile} />
+    </div>
+  )
 }
 
 export default App
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faChevronLeft from '@fortawesome/fontawesome-free-solid/faChevronLeft'
 
 const tunes = [
   'Big Sciota',
@@ -11,19 +13,35 @@ const tunes = [
 ]
 
 class SideNav extends Component {
+  state = {collapsed: true}
+
+  componentDidMount () {
+    // show menu when loading to home page
+    if (window.location.href.indexOf('tunes') === -1) {
+      this.setState({collapsed: false})
+    }
+  }
+
+  handleToggleClick = () => {
+    this.setState({collapsed: !this.state.collapsed})
+  }
+
   render() {
     let tuneList = tunes.map((tune, key) => {
       let tuneFile = tune.replace(/ /g, '_').replace(/'/g, '');
       return <Link key={key} to={"/tunes/"+tuneFile}><li>{tune}</li></Link>
     })
-    let sideNavClass = (this.props.collapsed) ? 'sideNav collapsed' : 'sideNav';
+    let sideNavClass = (this.state.collapsed) ? 'sideNav collapsed' : 'sideNav';
     return (
-        <div className={sideNavClass}>
-          <div className="menu">
-            <Link to="/"><li>Home</li></Link>
-            { tuneList }
-          </div>
+      <div className={sideNavClass}>
+        <div className="menu">
+          <Link to="/"><li>Home</li></Link>
+          { tuneList }
         </div>
+        <div className="toggleButtonContainer">
+          <span className="toggleButton" onClick={this.handleToggleClick}><FontAwesomeIcon icon={faChevronLeft} /></span>
+        </div>
+      </div>
     )
   }
 }
